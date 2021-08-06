4df46df05a41c5d8405ec91154842fbe51f39ea0
Yanis <59359557+yanis-man@users.noreply.github.com>
2021-08-06 16:26:38 +0200

    POO included
    The templater is now a class to allow futures built-in features.

--- templater.js
+++ templater.js
@@ -1,18 +1,41 @@
-async function loadHTML(url)
+class Templater
 {
-    return await (await fetch(url)).text();
+    constructor()
+    {
+        this.HTMLTemplate = ""
+    }
+    async loadHTML(url)
+    {
+        this.HTMLTemplate = await (await fetch(url)).text()
+    }
+    async replace(url, data)
+    {
+        await this.loadHTML(url)
+        let reg; 
+        let finalHTML;
+        Object.keys(data).forEach(key => {
+            reg = new RegExp(`{{${key}}}`, 'g')
+            finalHTML = this.HTMLTemplate.replaceAll(reg, data[key])
+        })
+        return finalHTML;
+    }
+    async mAll(url, data)
+    {
+        await this.loadHTML(url)
+        let reg;
+        let finalHTML ="";
+        let tempHTMLTemplate = "";
+        for(let i=0;i<data.length+1;i++)
+        {
+            tempHTMLTemplate = this.HTMLTemplate;
+            Object.keys(data[i]).forEach(key => {
+                reg = new RegExp(`{{${key}}}`, 'g')
+                console.log(key)
+                finalHTML += tempHTMLTemplate.replaceAll(reg, data[i][key])
+            })
+            return finalHTML;
+        }
+    }
 }
 
-async function replace(url, data)
-{
-
-    let templateHTML = await loadHTML(url);
-    let reg;
-    Object.keys(data).forEach(key => {
-        reg = new RegExp(`{{${key}}}`, 'g')
-        templateHTML = templateHTML.replaceAll(reg, data[key])
-    })
-    return templateHTML;
-}
-
-export {replace};
+export {Templater};
